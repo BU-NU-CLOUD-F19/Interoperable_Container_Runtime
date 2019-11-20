8a4196bd65d7427226f43cdacbafa22a2e1ab360
Jonathan Chamberlain <jdchambo@bu.edu>
2019-11-19 23:04:17 -0500

    Add files via upload

--- code/interoperable_image_app.py
+++ code/interoperable_image_app.py
@@ -4,38 +4,8 @@ from pyfiglet import Figlet
 from colorama import Fore, Back, Style 
 import os
 
+# define custom app font
 custom_fig = Figlet(font='doom')
-# help_text = "\n\nPlease enter command as --{runtime} {container_id} to apply benchmark tests on the associated container image\n\nExample: interoperable_image_app --crio 123134"
-
-# Configuration paths and attributes, Docker
-config_path_docker = "/var/lib/docker/containers/{}/config.v2.json"
-image_file_path_docker = "/var/lib/docker/image/overlay2/imagedb/content/sha256/{}"
-image_hash_id_docker = "Image"
-
-
-# Configuration paths and attributes, CRI-O
-config_path_crio = "/var/lib/containers/storage/overlay-containers/{}/userdata/config.json"
-container_annotation_crio = "annotations"
-container_image_ref_crio = "io.kubernetes.cri-o.ImageRef"
-
-image_overlay_crio = "/var/lib/containers/storage/overlay-images/images.json"
-image_overlay_digest = "digest"
-image_overlay_id = "id"
-image_file_path_crio = "/var/lib/containers/storage/overlay-images/{}/manifest"
-
-# Configuration paths and attributes, containerd
-
-image_file_ctrd = "/var/lib/containerd/io.containerd.content.v1.content/blobs/sha256/{}"
-
-# image file attributes
-config_key = "config" # holds basic configuration attributes
-user_key = "User" # User id, if present, found in config
-health_key = "Healthcheck" # Healthcheck instruction, if present, found in config
-history_key = "history" # holds updates to the underlying image
-created_key = "created" # timestamp of instruction
-created_by_key = "created_by" # instruction that caused the update
-
-trust_key = 'DOCKER_CONTENT_TRUST' # key for enviornment variable
 
 # Benchmark check strings
 Fp1 = "CIS 4.1: Create a user for the conatiner: "
@@ -47,7 +17,7 @@ Fp9 = "CIS 4.9: Use COPY instead of ADD in Dockerfile: "
 # Define functions to execute each check
 def userid_check(userid):
 	# Perform check 4.1, create a user
-	if userid == 'NOT SET!':
+	if userid == 'NOT SET!' or userid == 0:
 		print(f"{Fp1} " + Fore.RED + f"FAILURE, USER ID {userid}")
 	else:
 		print(f"{Fp1} " + Fore.GREEN + f"PASS, USER ID = {userid}")
@@ -92,6 +62,7 @@ def trusted_image_check(imageid):
 	print(Style.RESET_ALL + '')
 
 def content_trust_check():
+	trust_key = 'DOCKER_CONTENT_TRUST' # key for enviornment variable
 	#Perform check 4.5, enable trust - tied to enviornment variable
 	content_trust = os.environ.get(trust_key,'')
 	if content_trust == '1':
@@ -108,11 +79,13 @@ def healthcheck_check(healthcheck):
 		print(f"{Fp6} " + Style.GREEN + f"PASS, HEALTHCHECK INSTRUCTIONS {healthcheck}")
 	print(Style.RESET_ALL + '')
 
-def history_check(history):
+def history_check_docker(history):
 	# Perform check 4.9, use COPY not ADD
 	print(f'{Fp9}')
 	success = False # flag to mark if check passes
 	failure = False # flag to mark if check fails
+	created_key = "created" # timestamp of instruction
+	created_by_key = "created_by" # instruction that caused the update
 	add_times = [] # list to hold timestamps of add instructions
 	for line in history:
 		if 'ADD' in line[created_by_key]:
@@ -127,8 +100,40 @@ def history_check(history):
 	elif success:
 		print(Fore.GREEN + f'PASS, COPY AND NO ADD INSTRUCTIONS FOUND IN HISTORY')
 	else: 
-		print(Fore.Yellow + f'N/A, NO COPY OR ADD INSTRUCTIONS IN HISTORY')
+		print(Fore.YELLOW + f'N/A, NO COPY OR ADD INSTRUCTIONS IN HISTORY')
 	print(Style.RESET_ALL + '')
+
+def history_check_crio(history):
+	# Perform check 4.9, use COPY not ADD
+	print(f'{Fp9}')
+	success = False # flag to mark if check passes
+	failure = False # flag to mark if check fails
+	history_compatible_key = "v1Compatibility"
+	created_key = "created" # timestamp of instruction
+	config_key = "container_config" # contains instruciton in sub attribute
+	command_key = "cmd" # contains the instructions
+	add_times = [] # list to hold timestamps of add instructions
+	i = 0
+	for line in history:
+		if i > 0:
+			print(line[history_compatible_key][config_key])
+		'''
+		if 'ADD' in line[history_compatible_key][config_key][command_key]:
+			add_times.append(line[created_key])
+			failure = True
+		elif 'COPY' in line[created_key]:
+			success = True
+		'''
+		i += 1
+	if failure:
+		print(Fore.RED + f'FAILURE, ADD INSTRUCTIONS FOUND IN HISTORY AT:')
+		for t in add_times: print(t)
+	elif success:
+		print(Fore.GREEN + f'PASS, COPY AND NO ADD INSTRUCTIONS FOUND IN HISTORY')
+	else: 
+		print(Fore.YELLOW + f'N/A, NO COPY OR ADD INSTRUCTIONS IN HISTORY')
+	print(Style.RESET_ALL + '')
+
 
 # helper to print help function
 def help_func():
@@ -144,14 +149,17 @@ def help_func():
 
 # define checks in Docker
 def docker_inspect_image():
-	global config_path_docker
-	global image_file_path_docker
-	global image_hash_id_docker
-	global config_key
-	global user_key
-	global health_key
-	global history_key
-	global created_key
+	# Docker file paths
+	#global config_path_docker
+	#global image_file_path_docker
+	config_path_docker = "/var/lib/docker/containers/{}/config.v2.json"
+	image_file_path_docker = "/var/lib/docker/image/overlay2/imagedb/content/sha256/{}"
+	# attribute keys - in image file except for the hash key, which is in the config file
+	image_hash_key = "Image" # holds sha has of the image
+	config_key = "config" # holds basic configuration attributes
+	user_key = "User" # User id, if present, found in config
+	health_key = "Healthcheck" # Healthcheck instruction, if present, found in config
+	history_key = "history" # holds updates to the underlying image
 
 	if sys.argv[1] and sys.argv[1] == "--docker" or sys.argv[1] == "--d":
 		config_path_docker = config_path_docker.format(sys.argv[2])
@@ -159,7 +167,7 @@ def docker_inspect_image():
 		with open(config_path_docker) as config_json:
 			config_data = json.load(config_json)
 			# hash includes 'sha256:' to indicate hashtype, split off since image folder only includes ID
-			image_id = config_data[image_hash_id_docker].split(':')[1]
+			image_id = config_data[image_hash_key].split(':')[1]
 			# format image file path
 			image_file_path_docker = image_file_path_docker.format(image_id)
 			with open(image_file_path_docker) as image_json:
@@ -175,32 +183,38 @@ def docker_inspect_image():
 				trusted_image_check(image_id)
 				content_trust_check()
 				healthcheck_check(healthcheck)
-				history_check(history)			
+				history_check_docker(history)			
 		return True
 	return False
 
 def crio_inspect_image():
-	global config_path_crio
-	global container_annotation_crio
-	global container_image_ref_crio
-	global image_overlay_crio
-	global image_overlay_digest
-	global image_overlay_id
-	global image_file_path_crio 
-	global config_key
-	global user_key
-	global health_key
-	global history_key
-	global created_key
+	# paths to configuration files
+	config_path_crio = "/var/lib/containers/storage/overlay-containers/{}/userdata/config.json"
+	image_overlay_crio = "/var/lib/containers/storage/overlay-images/images.json"
+	image_file_path_crio = "/var/lib/containers/storage/overlay-images/{}/manifest"
+	# keys to attributes in the configuration file
+	config_process_key = "process"
+	user_key = "user"
+	user_id_key = "uid" 
+	container_annotation_crio = "annotations"
+	container_image_ref_crio = "io.kubernetes.cri-o.ImageRef"
+	# keys to attributes in the image overlay file
+	image_overlay_digest = "digest"
+	image_overlay_id = "id"
+	# keys to attributes in image file
+	history_key = "history"
 
 	if sys.argv[1] and sys.argv[1] == "--crio" or sys.argv[1] == "--c":
 		config_path_crio = config_path_crio.format(sys.argv[2])
 		# open crio config file to get digest of associated image
 		with open(config_path_crio) as config_json:
 			config_data = json.load(config_json)
+			# user id contained within process attribute
+			userid = config_data[config_process_key][user_key][user_id_key]
+			userid_check(userid)
 			# configuration file stores digest reference, but this is not image id
 			image_digest = config_data[container_annotation_crio][container_image_ref_crio].split('@')[1]
-			# have digest hash, use this to indentify container id
+			# have digest hash, use this to indentify image id
 			with open(image_overlay_crio) as image_overlay_json:
 				images_overlay_data = json.load(image_overlay_json)
 				# images stored as an unlabeled list
@@ -212,23 +226,32 @@ def crio_inspect_image():
 					if image[image_overlay_digest] == image_digest:
 						image_id = image[image_overlay_id]
 					i += 1
+				# have the id of the image, run trusted image check	
+				trusted_image_check(image_id)
 				# have image id, can now open relevant file
 				image_file_path_crio = image_file_path_crio.format(image_id)
 				with open(image_file_path_crio) as image_json:
 					image_data = json.load(image_json)
-					# userid and healthcheck are sub attributes of config attribute
-					image_config = image_data[config_key]
-					userid = image_config[user_key] if image_config[user_key] is not '' else 'NOT SET!'
-					healthcheck = image_config[health_key] if health_key in image_config else 'NOT SET!'
 					# history in its own attribute
 					history = image_data[history_key]
+					# history_check_crio(history)
+					# healthcheck attribute ???? of config attribute
+					#healthcheck = image_config[health_key] if health_key in image_config else 'NOT SET!'
 					# print result of each check
-					userid_check(userid)
-					#trusted_image_check(image_id)
 					#content_trust_check()
-					healthcheck_check(healthcheck)
-					history_check(history)			
+					#healthcheck_check(healthcheck)
+							
 		return True
+	return False
+
+def ctrd_inspect_image():
+
+	#image_file_ctrd = "/var/lib/containerd/io.containerd.content.v1.content/blobs/sha256/{}"
+
+	if sys.argv[1] and sys.argv[1] == "--containerd" or sys.argv[1] == "--ctrd":
+		print ("containerd is not currently supported")
+		return True
+
 	return False
 
 def main():
